d5ea673fc2fc3f3345fcb925f59c52efa0f42c13
Jones Pham <phamvankhang.tvi@gmail.com>
2022-06-26 02:58:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,23 +31,6 @@ const generate = async () => {
     `
   ## Hi there,👋👋 I'm Phạm Văn Khang 
 
-<p align="center">
-<img width="20%" src="https://github.com/vkhang-stack/vkhang-stack/blob/master/zebra.png"/>
-</p>
-
-
-- 🔭 I'm **Student** | **Coder** | **Backend Developer** | **Hate Code**
-- 🌱 Currently Learning **JavaScript**, **Web Development**
-- ⚡ Hobbies: Reading Book, listening to music, playing game League of Legends
-
-### 💬📫 Feel Free to Contact me.....
-
-<p align="center">
-	<a href="https://github.com/vkhangstack"><img alt="github" width="10%" style="padding:5px" src="https://img.icons8.com/clouds/100/000000/github.png"/></a>
-	<a href="https://www.facebook.com/vkhang.pvkteam/"><img alt="facebook" width="10%" style="padding:5px" src="https://img.icons8.com/clouds/100/000000/facebook-new.png"/></a>
-	<a href="https://twitter.com/hx10r"><img alt="twitter" width="10%" style="padding:5px" src="https://img.icons8.com/clouds/100/000000/twitter.png"/></a>
-</p>
-
   _**${quote}**_\n\n_${author}`,
   );
 };
