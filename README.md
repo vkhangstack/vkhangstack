7a4a999bd6a11cd57072fa11fda156f50e5a6647
Khang Pham <khang.pv@intelin.vn>
2022-11-06 21:39:30 +0700

    update info profile github

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 
-  ## Github Profile of Pham Van Khang
-[![trophy](https://github-profile-trophy.vercel.app/?username=vkhangstack)](https://github.com/vkhangstack/vkhangstack)  
-<img align="center" src="https://github-readme-stats.vercel.app/api/?username=vkhangstack&theme=dracula" />
+     ## Github Profile of Pham Van Khang
+      {
+        "name": "Pham Van Khang",
+        "job: "Fullstack Developer",
+        "portfolio": "https://phamvankhang.name.vn",
+        "email": "phamvankhang.tvi@gmail.com",
+        "github": "https://github.com/vkhangstack",
+        "linkedin": "https://www.linkedin.com/in/vkhangstack",
+        "description": "As a dynamic person and interested in learning a lot of new 
+        knowledge, I wanna improve my English language and programming skills to 
+        become a professional developer.",
+      }
+  
+  [![trophy](https://github-profile-trophy.vercel.app/?username=vkhangstack)](https://github.com/vkhangstack/vkhangstack)
+  <img align="center" src="https://github-readme-stats.vercel.app/api/?username=vkhangstack&theme=dracula" />
 
-  _**Never let the fear of striking out keep you from playing the game.**_
-
-_Babe Ruth
+     He who is contented is rich. - Laozi
--- index.js
+++ index.js
@@ -1,40 +1,51 @@
-const axios = require("axios");
-const fs = require("fs");
+const axios = require("axios")
+const fs = require("fs")
 
 const getQuote = async () => {
   try {
-    const { data } = await axios.get(
-      "https://api.quotable.io/random"
-    );
-    const quote = data.response.quote;
-    const author = data.response.author;
+    const { data } = await axios.get("https://api.quotable.io/random")
+    console.log(data)
+    const quote = data.content
+    const author = data.author
 
-    console.log("new quote", `"${quote}" - ${author}`);
+    console.log("new quote", `"${quote}" - ${author}`)
 
     return {
       quote,
       author,
-    };
+    }
   } catch (err) {
-    console.error(err.message);
-    return {};
+    console.error(err.message)
+    return {}
   }
-};
+}
 
 const generate = async () => {
-  const { quote, author } = await getQuote();
+  const { quote, author } = await getQuote()
 
-  if (!quote) return;
+  if (!quote) return
 
   fs.writeFileSync(
     "README.md",
     `
-  ## Github Profile of Pham Van Khang
-[![trophy](https://github-profile-trophy.vercel.app/?username=vkhangstack)](https://github.com/vkhangstack/vkhangstack)  
-<img align="center" src="https://github-readme-stats.vercel.app/api/?username=vkhangstack&theme=dracula" />
+     ## Github Profile of Pham Van Khang
+      {
+        "name": "Pham Van Khang",
+        "job: "Fullstack Developer",
+        "portfolio": "https://phamvankhang.name.vn",
+        "email": "phamvankhang.tvi@gmail.com",
+        "github": "https://github.com/vkhangstack",
+        "linkedin": "https://www.linkedin.com/in/vkhangstack",
+        "description": "As a dynamic person and interested in learning a lot of new 
+        knowledge, I wanna improve my English language and programming skills to 
+        become a professional developer.",
+      }
+  
+  [![trophy](https://github-profile-trophy.vercel.app/?username=vkhangstack)](https://github.com/vkhangstack/vkhangstack)
+  <img align="center" src="https://github-readme-stats.vercel.app/api/?username=vkhangstack&theme=dracula" />
 
-  _**${quote}**_\n\n_${author}`
-  );
-};
+     ${quote} - ${author}`
+  )
+}
 
-generate();
+generate()
